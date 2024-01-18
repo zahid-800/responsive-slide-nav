d3f9f46f731c566f35458b75dd989093cb21427d
zahid-800 <154730312+zahid-800@users.noreply.github.com>
2024-01-18 17:40:17 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function showSidebar(){
+  const sidebar= document.getElementById('sidebar').style.display='flex'
+}
+function hideSidebar(){
+  const sidebar= document.getElementById('sidebar').style.display='none'
+}
+
